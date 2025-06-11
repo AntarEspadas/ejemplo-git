9bc55b03f5e81951f61cf499775e1ffaf99ce0e2
Antar Espadas <antar.espadas@hotmail.com>
2025-06-10 18:15:47 -0600

    Crear archivo funciones.c

--- hola_mundo.c
+++ hola_mundo.c
@@ -1,19 +1,8 @@
 // Autor: Antar Espadas
 
 #include <stdio.h>
-
-int suma(int n) {
-    if (n == 1) {
-       return 1;
-    }
-    return n + suma(n - 1);
-}
+#include "funciones.c"
 
 int main() {
     printf("Hola mundo!\n");
-}
-
-int factorial(int n) {
-    if (n == 0) return 1;
-    return n * factorial(n-1);
 }
